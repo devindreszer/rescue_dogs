f7e1af41b6833a7fe04568f559e0c9fa1fe202d3
Devin Dreszer <devindreszer@gmail.com>
2014-07-08 11:55:24 -0400

    User is redirected to dog page they were visiting after sign in

--- app/controllers/random_dogs_controller.rb
+++ app/controllers/random_dogs_controller.rb
@@ -2,6 +2,8 @@ class RandomDogsController < ApplicationController
 
   def show
     @random_dog = Dogfinder.new.random(params[:zip])
+    session[:dog_id] = @random_dog.id
+    session[:zip] = params[:zip]
   end
 
 end

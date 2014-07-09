1a683ab14a2eb5ee00ad96936215a82c957cde1f
Devin Dreszer <devindreszer@gmail.com>
2014-07-09 16:50:12 -0400

    Users have dog match cards
    * Dogs ar now saved into the database with size and gender spelled out

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,6 @@
 class Dog < ActiveRecord::Base
+  GENDERS = {"M" => "Male", "F" => "Female"}
+  SIZES = {"S" => "Small", "M" => "Medium", "L" => "Large"}
   belongs_to :shelter
   has_many :dog_matches, dependent: :destroy
   has_many :users, through: :dog_matches
@@ -7,8 +9,8 @@ class Dog < ActiveRecord::Base
     self.petfinder_id = petfinder_dog.id
     self.name = petfinder_dog.name
     self.image = petfinder_dog.photos.first.large
-    self.size = petfinder_dog.size
-    self.gender = petfinder_dog.sex
+    self.size = SIZES[petfinder_dog.size]
+    self.gender = GENDERS[petfinder_dog.sex]
     self.age = petfinder_dog.age
     self.description = petfinder_dog.description
     self.url = "https://www.petfinder.com/petdetail/#{petfinder_dog.id}"

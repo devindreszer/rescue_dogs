c8cf4424e686f0003e36b1e41a57ec03d97373e9
Devin Dreszer <devindreszer@gmail.com>
2014-07-09 10:24:20 -0400

    Users can favorite a dog from their info page

--- app/controllers/dog_matches_controller.rb
+++ app/controllers/dog_matches_controller.rb
@@ -1,11 +1,12 @@
 class DogMatchesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     if params[:favorite]
-      @dog_matches = current_user.dog_matches.where(is_favorite: true) if user_signed_in?
+      @dog_matches = current_user.dog_matches.where(is_favorite: true)
       @dogs = @dog_matches.map(&:dog)
     else
-      @dog_matches = current_user.dog_matches if user_signed_in?
+      @dog_matches = current_user.dog_matches
       @dogs = @dog_matches.map(&:dog)
     end
   end
--- spec/features/user_favorites_dog_match_spec.rb
+++ spec/features/user_favorites_dog_match_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 feature 'Signed in user can favorite a dog' do
-  scenario 'by clicking favorite on a dog match' do
+  background do
     devin = create(:user, username: "devin", email: "devin@example.com", password: "password")
     visit root_path
     sign_in_as(devin)
     fill_in "Enter Zip Code", with: "02093"
     click_button "Search"
     click_link "Plus"
+  end
+
+  scenario 'by clicking favorite on a dog match' do
     click_link "Dog Matches"
 
     first(".dog-list").click_link("Favorite")
+
+    expect(page).to have_content "Favorited"
+  end
+
+  scenario 'by clicking favorite from a dog info page' do
+    click_link "Favorite"
 
     expect(page).to have_content "Favorited"
   end

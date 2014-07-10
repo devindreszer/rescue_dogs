56672badc7ae4149159648b3649b20f8165d441c
Devin Dreszer <devindreszer@gmail.com>
2014-07-10 09:52:13 -0400

    Users can view explanation of features and stats on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
   def show
+    @user_count = User.all.count
+    @dog_matches = DogMatch.all.count
+    @dog_favorites = DogMatch.where(is_favorite: true).count
   end
+
 end

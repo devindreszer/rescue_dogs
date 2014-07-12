3313b769f0c679d548250527c29a30d12a9b3042
Devin Dreszer <devindreszer@gmail.com>
2014-07-12 09:57:37 -0400

    Random dog selection is done in the controller

--- app/controllers/random_dogs_controller.rb
+++ app/controllers/random_dogs_controller.rb
@@ -1,18 +1,31 @@
 class RandomDogsController < ApplicationController
 
   def show
-    dogfinder = Dogfinder.new
-    @random_dog = dogfinder.random(params[:zip])
+    zip = params[:zip]
+    @random_dogs = Dog.where(last_zip: zip)
 
-    # check for errors after random method is called
-    if dogfinder.zip_error.present?
-      flash[:alert] = dogfinder.zip_error
-      redirect_to "/"
-    else
-      session[:dog_id] = @random_dog.id
-      session[:zip] = params[:zip]
+    # check to see if there are enough dogs in the database with the zip passed
+    # if not make an api request to collect more dogs in the database
+    if @random_dogs.nil? || @random_dogs.count < Dogfinder::COUNT.to_i
+      dogfinder = Dogfinder.new
+      @random_dogs = dogfinder.collect(zip)
+
+      # check for errors after collect method is called
+      # to see if the zip is valid
+      if dogfinder.zip_error.present?
+        flash[:alert] = dogfinder.zip_error
+        return redirect_to "/"
+      end
     end
 
+    # Sample a random dog from the collection
+    @random_dog = @random_dogs.sample
+
+    # Set session hash with the dog id and the zip called
+    session[:dog_id] = @random_dog.id
+    session[:zip] = params[:zip]
+
+    # Set a fortune cookie message for the dog
     @message = Message.new.get
   end
 
--- app/models/dogfinder.rb
+++ app/models/dogfinder.rb
@@ -3,37 +3,8 @@ class Dogfinder
 
   attr_accessor :zip_error
 
-  def random(zip)
-    if Dog.find_by(last_zip: zip).nil? || Dog.where(last_zip: zip).count < COUNT.to_i
-      dogs = collect(zip)
-
-      # If an error is raised in collect; dogs will be nil
-      if dogs.present?
-        dogs.sample
-      else
-        return
-      end
-
-    else
-      Dog.where(last_zip: zip).sample
-    end
-  end
-
-  def find(id)
-    client.pet(id)
-  end
-
-  def shelter(shelter_id)
-    client.shelter(shelter_id)
-  end
-
-  private
-
-  def client
-    Petfinder::Client.new(ENV['PETFINDER_KEY'], ENV['PETFINDER_SECRET'])
-  end
-
   def collect(zip)
+    # Collect COUNT number of dogs from database
     # Catch invalid zip codes; set zip_error and return nil
     begin
       petfinder_dogs = client.find_pets("dog", zip, count: COUNT)
@@ -44,6 +15,8 @@ class Dogfinder
 
     @dogs = []
 
+    # Add petfinder dogs and their shelters to the database
+    # Then add it to the dog array returned to the controller
     petfinder_dogs.each do |petfinder_dog|
       petfinder_shelter = client.shelter(petfinder_dog.shelter_id)
       shelter = set_shelter(petfinder_shelter)
@@ -62,6 +35,20 @@ class Dogfinder
     end
 
     @dogs
+  end
+
+  def find(id)
+    client.pet(id)
+  end
+
+  def shelter(shelter_id)
+    client.shelter(shelter_id)
+  end
+
+  private
+
+  def client
+    Petfinder::Client.new(ENV['PETFINDER_KEY'], ENV['PETFINDER_SECRET'])
   end
 
   def set_shelter(petfinder_shelter)

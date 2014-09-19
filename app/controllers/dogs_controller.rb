2289593324b4530f48dd29f4de60da07c67ba21c
Devin Dreszer <devindreszer@gmail.com>
2014-09-19 08:48:07 -0600

    Bug fix to allow guest users to sign in after a match

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,7 +3,7 @@ class DogsController < ApplicationController
 
   def show
     @dog = Dog.find(params[:id])
-    @dog_query = current_user.dog_queries.find_by(dog_id: @dog.id)
+    @dog_query = current_or_guest_user.dog_queries.find_by(dog_id: @dog.id)
     @dog_query.mark_match(session[:fortune])
   end
 

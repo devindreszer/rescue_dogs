7760b322fbf38925e4b0f353d65ee65c113bc225
Devin Dreszer <devindreszer@gmail.com>
2014-11-18 19:29:45 -0700

    All dogs can be accessed; fortunes just will not show

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,7 +5,7 @@ class DogsController < ApplicationController
     @dog = Dog.find(params[:id])
     @user = current_or_guest_user
     @dog_query = @user.dog_queries.find_by(dog_id: @dog.id)
-    @dog_query.mark_match(session[:fortune])
+    @dog_query.mark_match(session[:fortune]) if @dog_query.present?
   end
 
 end
